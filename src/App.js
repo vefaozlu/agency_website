db495a95eb39706b0b847792b86c8a4ae74328ad
Vefa Ozlu <vefaozlu01@gmail.com>
2024-06-20 14:08:40 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import GlobalStyles from "styles/GlobalStyles";
+import "../node_modules/tailwindcss/lib/css/preflight.css";
+import "tailwindcss/lib/css/preflight.css";
+
+import Hero from "components/hero/TwoColumnWithInput";
+import Features from "components/features/TwoColSingleFeatureWithStats2";
+import Cards from "components/cards/ThreeColContactDetails";
+import Testimonial from "components/testimonials/SimplePrimaryBackground";
+
+import "tailwindcss/lib/css/preflight.css";
+import AnimationRevealPage from "helpers/AnimationRevealPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AnimationRevealPage disabled>
+        <GlobalStyles />
+        <Hero />
+        <Features />
+        <Cards />
+        <Testimonial />
+      </AnimationRevealPage>
     </div>
   );
 }
